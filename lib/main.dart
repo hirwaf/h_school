c85fd282e509ab75e70f97ece49687501ccecf06
Hirwa Felix <hirwa.felix@gmail.com>
2019-07-01 22:21:16 +0200

    Add buttons ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hschool/utils/connectionStatusSingleton.dart';
 
 import 'app.dart';
 
-void main() => runApp(App());
+void main() {
+  ConnectionStatusSingleton connectionStatus =
+      ConnectionStatusSingleton.getInstance();
+  connectionStatus.initialize();
+
+  runApp(App());
+}
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:hschool/utils/connectionStatusSingleton.dart';
 
 class HomePage extends StatefulWidget {
   static final String routeName = 'home';
@@ -13,28 +14,33 @@ class HomePage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => _HomeState();
-
 }
 
 class _HomeState extends State<HomePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  bool _internet = false;
-
-  StreamSubscription<ConnectivityResult> subscription;
+  StreamSubscription _connectionChangeStream;
+  bool isOffline = false;
 
   @override
   initState() {
     super.initState();
-    subscription = Connectivity().onConnectivityChanged.listen((
-        ConnectivityResult result) {
-      _internet = true;
+
+    ConnectionStatusSingleton connectionStatus =
+        ConnectionStatusSingleton.getInstance();
+    _connectionChangeStream =
+        connectionStatus.connectionChange.listen(connectionChanged);
+  }
+
+  void connectionChanged(dynamic hasConnection) {
+    setState(() {
+      isOffline = !hasConnection;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    print(subscription);
+
     return SafeArea(
       child: new Scaffold(
         key: _scaffoldKey,
@@ -42,7 +48,7 @@ class _HomeState extends State<HomePage> {
           title: Text('${widget.title}'),
           elevation: 5.5,
         ),
-        body: _internet ? _body() : _loading(),
+        body: !isOffline ? _body() : _loading(),
         backgroundColor: Color.fromRGBO(250, 250, 250, 1),
       ),
     );
@@ -50,30 +56,72 @@ class _HomeState extends State<HomePage> {
 
   Widget _body() {
     return Container(
-      child: Column(
-        children: <Widget>[
-        ],
+      child: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text(
+                "New Application",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {},
+              padding: EdgeInsets.all(10),
+              elevation: 5.2,
+            ),
+            const SizedBox(height: 30),
+            RaisedButton(
+              child: Text(
+                "Students Portal",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {},
+              padding: EdgeInsets.all(10),
+              elevation: 5.2,
+            ),
+            const SizedBox(height: 30),
+            RaisedButton(
+              child: Text(
+                "Lecturers Portal",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {},
+              padding: EdgeInsets.all(10),
+              elevation: 5.2,
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget _loading() {
-    return SpinKitFadingCircle(
-      itemBuilder: (_, int index) {
-        return DecoratedBox(
-          decoration: BoxDecoration(
-            color: index.isEven ? Colors.red : Colors.green,
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text(
+              "There is not internet connection !!!",
+              style: TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600),
+            ),
           ),
-        );
-      },
+          SpinKitFadingCircle(
+            itemBuilder: (_, int index) {
+              return DecoratedBox(
+                decoration: BoxDecoration(
+                  color: index.isEven ? Colors.red : Colors.green,
+                ),
+              );
+            },
+          )
+        ],
+      ),
     );
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    subscription.cancel();
-  }
-
 }
